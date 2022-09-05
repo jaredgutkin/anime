2cc144af9d54277e61b93cd113e5bcd68eeb7995
Jared Gutkin <jgutkinsam@gmail.com>
2022-09-05 16:21:24 -0500

    anime infor set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react"
 import AnimeList from "./components/AnimeList"
+import { AnimeInfo } from "./components/AnimeInfo"
 import './components/style.css'
 
 function App() {
 
   const [search, setSearch] = useState('Dragon Ball')
   const [animeData, setAnimeData] = useState()
+  const [animeInfo, setAnimeInfo] = useState()
 
   const getData = async () => {
     const res = await fetch(`https://api.jikan.moe/v4/anime?q=${search}&limit=20`)
@@ -30,12 +32,15 @@ function App() {
 
       <div className="container">
         <div className="animeInfo">
-
+          {animeInfo && <AnimeInfo animeInfo={animeInfo} />}
         </div>
         <div className="anime-row">
           <h2 className="text-heading">Anime</h2>
           <div className="row">
-            <AnimeList animelist={animeData} />
+            <AnimeList 
+              animelist={animeData}
+              setAnimeInfo={setAnimeInfo}
+             />
           </div>
         </div>
       </div>
--- src/components/AnimeList.jsx
+++ src/components/AnimeList.jsx
@@ -1,12 +1,12 @@
 
-export default function AnimeList({animelist}) {
+export default function AnimeList({animelist, setAnimeInfo}) {
   return (
     <>
     {
         animelist ?(
             animelist.map((anime, index) =>{
                 return(
-                    <div className="card">
+                    <div className="card" key={index} onClick={() => setAnimeInfo(anime)}>
                         <img src={anime.images.jpg.large_image_url} alt="animeImage" />
                         <div className="anime-info">
                             <h4>{anime.title}</h4>
