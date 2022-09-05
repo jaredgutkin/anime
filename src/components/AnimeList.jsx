50125c96a897f2fc0eb1d00e38128011ee381317
Jared Gutkin <jgutkinsam@gmail.com>
2022-09-05 16:42:55 -0500

    set up overlay

--- src/components/AnimeList.jsx
+++ src/components/AnimeList.jsx
@@ -10,6 +10,13 @@ export default function AnimeList({animelist, setAnimeInfo}) {
                         <img src={anime.images.jpg.large_image_url} alt="animeImage" />
                         <div className="anime-info">
                             <h4>{anime.title}</h4>
+                            <div className="overlay">
+                                <h4>{anime.title_japanese}</h4>
+                                <h3>SYNOPSIS</h3>
+                                <div className="synopsis">
+                                    <p>{anime.synopsis}</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 )
